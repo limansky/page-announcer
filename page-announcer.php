8d051b4a440f8abc64d02b188660315e12f6a937
Mikhail Limansky <mike.limansky@gmail.com>
2016-06-04 21:43:27 +0300

    Support three different image styles: circle, rounded corners and none.

--- page-announcer.php
+++ page-announcer.php
@@ -3,7 +3,7 @@
 Plugin Name: Page announcer
 Plugin URI:  http://github.com/limansky/page-announcer
 Description: A widget to show a short description for a page, e.g. about yourself box.
-Version:     0.1
+Version:     0.2
 Author:      Mike Limansky
 Author URI:  http://www.limansky.me
 License:     GPL2
@@ -11,6 +11,13 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 class PageAnnouncer extends WP_Widget {
+
+    const IMAGE_STYLES = array(
+        'circle'            => array('Circle',              'widget_page_announcer_image_circle'),
+        'round-corners'     => array('Rounded corners',     'widget_page_announcer_image_round_corners'),
+        'none'              => array('No effect',           'widget_page_announcer_image')
+    );
+
     function __construct() {
         parent::__construct(
             'page_announcer_widget',
@@ -37,11 +44,13 @@ class PageAnnouncer extends WP_Widget {
             $link = esc_attr($instance['link']);
             $title = esc_attr($instance['title']);
             $image = esc_attr($instance['image']);
+            $image_style = esc_attr($instance['image_style']);
         } else {
             $title = '';
             $link = '';
             $image = '';
             $text = '';
+            $image_style = 'circle';
         }
 
         ?>
@@ -57,6 +66,18 @@ class PageAnnouncer extends WP_Widget {
             <label for="<?php echo $this->get_field_id('image'); ?>"><?php _e('Image:', page_announcer);?></label>
             <input class="widefat" type="text" id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>" value="<?php echo $image;?>" >
             <button class="button button-primary page-announcer-load-image-button">Upload</button>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('image_style'); ?>"><?php _e('Image style:', page_announcer);?></label>
+            <select class="widefat" id="<?php echo $this->get_field_id('image_style'); ?>" name=<?php echo $this->get_field_name('image_style'); ?>" >
+                <?php
+                    foreach (self::IMAGE_STYLES as $style => $meta) {
+                        echo '<option value="' . $style . '"';
+                        if ($image_style == $style) echo ' selected ';
+                        echo '>' . __($meta[0], page_announcer) . '</option>';
+                    }
+                ?>
+            </select>
         </p>
         <p>
             <label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Target page:', page_announcer);?></label>
@@ -78,6 +99,7 @@ class PageAnnouncer extends WP_Widget {
         $instance['text'] = strip_tags($new_instance['text']);
         $instance['link'] = $new_instance['link'];
         $instance['image'] = $new_instance['image'];
+        $instance['image_style'] = $new_instance['image_style'];
         return $instance;
     }
 
@@ -87,6 +109,7 @@ class PageAnnouncer extends WP_Widget {
         $title = apply_filters('widget_title', $instance['title']);
         $text = $instance['text'];
         $image = $instance['image'];
+        $image_style = $instance['image_style'];
         $link = get_page_link($instance['link']);
 
         if ($title) {
@@ -94,7 +117,13 @@ class PageAnnouncer extends WP_Widget {
         }
 
         if ($link) echo '<a href="' . esc_url($link) .'" class="widget_page_announcer_link">';
-        if ($image) echo '<img src="' . esc_url($image) . '" class="widget_page_announcer_image_circle">';
+        if ($image) {
+            $style = 'widget_page_announcer_image';
+            if ($image_style && self::IMAGE_STYLES[$image_style]) {
+                $style = self::IMAGE_STYLES[$image_style][1];
+            }
+            echo '<img src="' . esc_url($image) . '" class="' . $style .'">';
+        }
         if ($text) echo '<p class="widget_page_announcer_text">' . $text . '</p>';
         if ($link) echo '</a>';
 
